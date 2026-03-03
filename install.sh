253561b9312799c6e6574b243911499068d65664
Thames Sookpranee <thames@hey.com>
2026-03-02 20:57:04 -0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+DOTFILES="$(cd "$(dirname "$0")" && pwd)"
+
+echo "Linking nvim..."
+ln -sfn "$DOTFILES/nvim" "$HOME/.config/nvim"
+
+echo "Linking tmux.conf..."
+ln -sf "$DOTFILES/tmux.conf" "$HOME/.tmux.conf"
+
+echo "Installing TPM (tmux plugin manager)..."
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+else
+  echo "TPM already installed, skipping."
+fi
+
+echo "Done."
