d149d09067959eb460757651454fb5333af4f040
Thames Sookpranee <thames@hey.com>
2026-03-02 20:49:38 -0800

    tokyonight

--- nvim/lua/plugins/thames.lua
+++ nvim/lua/plugins/thames.lua
@@ -1,5 +1,21 @@
 return {
   {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
     "folke/snacks.nvim",
     opts = {
       terminal = {
