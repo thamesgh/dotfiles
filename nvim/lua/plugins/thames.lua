acc34f18466fdcd1215d2bf4ff1a7ed22c1f1197
Thames Sookpranee <thames@hey.com>
2026-03-22 18:33:23 -0700

    add typescript formatter

--- nvim/lua/plugins/thames.lua
+++ nvim/lua/plugins/thames.lua
@@ -71,4 +71,14 @@ return {
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
   },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+      },
+      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    },
+  },
 }
