4f9338b687b2f2e6ae283d4db82b73ff36a87ca0
Thames Sookpranee <thames@hey.com>
2026-08-27 22:00:02 -0700

    update nvim to be omarchy-compatible

--- install.sh
+++ install.sh
@@ -4,8 +4,23 @@ set -e
 
 DOTFILES="$(cd "$(dirname "$0")" && pwd)"
 
-echo "Linking nvim..."
-ln -sfn "$DOTFILES/nvim" "$HOME/.config/nvim"
+echo "Linking nvim config and plugin files..."
+NVIM_CONFIG="$HOME/.config/nvim"
+
+if [ -L "$NVIM_CONFIG" ]; then
+  echo "$NVIM_CONFIG is a symlink; expected an existing Neovim config directory." >&2
+  echo "Restore the Omarchy Neovim config, then run this script again." >&2
+  exit 1
+fi
+
+for section in config plugins; do
+  mkdir -p "$NVIM_CONFIG/lua/$section"
+
+  for source in "$DOTFILES/nvim/lua/$section"/*.lua; do
+    [ -e "$source" ] || continue
+    ln -sfn "$source" "$NVIM_CONFIG/lua/$section/$(basename "$source")"
+  done
+done
 
 echo "Linking tmux.conf..."
 ln -sf "$DOTFILES/tmux.conf" "$HOME/.tmux.conf"
--- nvim/lua/plugins/thames.lua
+++ nvim/lua/plugins/thames.lua
@@ -1,94 +1,86 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "cyberdream",
-    },
-  },
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      variant = "auto",
-    },
-  },
-  { "projekt0n/github-nvim-theme", name = "github-theme" },
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-  {
-    "folke/snacks.nvim",
-    opts = {
-      indent = { enabled = false },
-      terminal = {
-        win = {
-          position = "float",
-          border = "rounded",
-        },
-      },
-      picker = {
-        sources = {
-          explorer = {
-            layout = {
-              layout = {
-                position = "right",
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        markdown = {},
-      },
-    },
-  },
-  {
-    "render-markdown.nvim",
-    opts = {
-      heading = {
-        enabled = false,
-      },
-    },
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-      "TmuxNavigatorProcessList",
-    },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        typescript = { "prettier" },
-        typescriptreact = { "prettier" },
-      },
-      format_on_save = { timeout_ms = 500, lsp_fallback = true },
-    },
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "cyberdream",
+		},
+	},
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			variant = "auto",
+		},
+	},
+	{ "projekt0n/github-nvim-theme", name = "github-theme" },
+	{
+		"folke/tokyonight.nvim",
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
+	{
+		"folke/snacks.nvim",
+		opts = {
+			indent = { enabled = false },
+			terminal = {
+				win = {
+					position = "float",
+					border = "rounded",
+				},
+			},
+			picker = {
+				sources = {
+					explorer = {
+						layout = {
+							layout = {
+								position = "right",
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		"mfussenegger/nvim-lint",
+		opts = {
+			linters_by_ft = {
+				markdown = {},
+			},
+		},
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+			"TmuxNavigatorProcessList",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+			},
+			format_on_save = { timeout_ms = 500, lsp_fallback = true },
+		},
+	},
 }
